3ac667e9febdaa02a273e6cda3523a620343607a
Loto <195737806+Loto417@users.noreply.github.com>
2025-09-02 11:10:07 +0200

    Update Libro.java
    libro.java v2

---
+++ Libro.java
@@ -0,0 +1,27 @@
+public class Libro {
+    private String titolo;
+    private String autore;
+    private int anno;
+
+    // costruttore vuoto
+    public Libro() {}
+
+    // costruttore completo
+    public Auto(String titolo, String autore, int anno, int prezzo) {
+        this.marca = marca;
+        this.modello = modello;
+        this.anno = anno;
+        this.prezzo = prezzo;
+    }
+
+    // getter e setter
+    public String getTitolo() { return titolo; }
+    public void setTitolo(String nuovoTitolo) { this.titolo = nuovoTitolo; }
+
+    public String getAutore() { return autore; }
+    public void setAutore(String nuovoAutore) { this.autoreo = nuovoAutore; }
+
+    public int getAnno() { return anno; }
+    public void setAnno(int nuovoAnno) { this.anno = nuovoAnno; }
+
+}
