75fc167da6cb66e23ef1c34bbc2bd4499310db0d
cdilallo-beep <cdilallo@itsmeccanicabruzzo.eu>
2025-09-02 11:39:04 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import model.Libro;
+import model.Utente;
+import service.BibliotecaService;
+
+public class Main {
+public static void main(String[] args) {
+Biblioteca biblioteca = new Biblioteca();
+
+    Libro libro1 = new Libro("Il nome della rosa", "Umberto Eco", 1980);
+    biblioteca.aggiungiLibro(libro1);
+
+    Utente utente = new Utente("Roberta", 1);
+    biblioteca.prestaLibro("Il nome della rosa", utente);
+
+    biblioteca.visualizzaLibriDisponibili();
+}
+}
