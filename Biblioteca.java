46c9b195d061124fdece3d0823e37c3e5c074ba7
rzavanelli-blip <rzavanelli@itsmeccanicabruzzo.eu>
2025-09-02 11:19:05 +0200

    Add files via upload

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class Biblioteca {
-    
+    private ArrayList<Libro> libri = new ArrayList<>();
+
+    public void aggiungiLibro(Libro libro) {
+        libri.add(libro);
+    }
+
+    public void visualizzaLibri() {
+        for (Libro libro : libri) {
+            System.out.println(libro);
+        }
+    }
+}
+
 }
